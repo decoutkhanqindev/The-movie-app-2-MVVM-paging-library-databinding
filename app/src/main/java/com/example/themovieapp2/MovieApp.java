d7ef54648864f3eb383da7fa1845c0d5a4b5a1e5
MeansKane <phamminhkhangpy2003@gmail.com>
2024-05-16 00:42:46 +0700

    The movie app 2 MVVM + paging library + databinding + retrofit + dagger hilt

---
+++ app/src/main/java/com/example/themovieapp2/MovieApp.java
@@ -0,0 +1,9 @@
+package com.example.themovieapp2;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MovieApp extends Application {
+}
---
+++ app/src/main/java/com/example/themovieapp2/activity/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.themovieapp2.activity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+import com.bumptech.glide.RequestManager;
+import com.example.themovieapp2.R;
+import com.example.themovieapp2.adapter.MovieLoadStateAdapter;
+import com.example.themovieapp2.adapter.MoviesPagingAdapter;
+import com.example.themovieapp2.databinding.ActivityMainBinding;
+import com.example.themovieapp2.utility.GridSpace;
+import com.example.themovieapp2.utility.MovieComparator;
+import com.example.themovieapp2.viewmodel.MovieViewModel;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint // danh dau main activity la diem nhap cho Hilt them cac phu thuoc cua no
+public class MainActivity extends AppCompatActivity {
+    MovieViewModel movieViewModel; // la 1 viewmodel chua list movies
+    ActivityMainBinding mainBinding; // la 1 bien dc tao ra boi Databinding dc su dung de truy cap
+    MoviesPagingAdapter moviesPagingAdapter;
+    RequestManager requestManager;
+
+    @SuppressLint("CheckResult")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        moviesPagingAdapter = new MoviesPagingAdapter(new MovieComparator(), requestManager);
+
+        movieViewModel = new ViewModelProvider(this).get(MovieViewModel.class);
+
+        initRecyclerviewAndAdapter();
+
+        // subscribe to paging data
+        movieViewModel.moviePagingDataFlowable.subscribe(moviePagingData -> {
+            moviesPagingAdapter.submitData(getLifecycle(), moviePagingData);
+        });
+    }
+
+    private void initRecyclerviewAndAdapter() {
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        mainBinding.recyclerview.setLayoutManager(gridLayoutManager);
+        mainBinding.recyclerview.addItemDecoration(new GridSpace(2, 20, true));
+        mainBinding.recyclerview.setAdapter(moviesPagingAdapter.withLoadStateFooter(
+                new MovieLoadStateAdapter(view -> {
+                    moviesPagingAdapter.retry();
+                })
+        ));
+
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                return moviesPagingAdapter.getItemViewType(position) == MoviesPagingAdapter.LOAD_ITEM ? 1 : 2;
+            }
+        });
+    }
+}
